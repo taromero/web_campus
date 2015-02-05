1672a8ba1e02e2eefaff9f232010c6282034b9c8
Tomas Romero <canotto90@gmail.com>
2015-02-05 02:19:56 -0300

    Added seeds file

---
+++ seeds.js
@@ -0,0 +1,35 @@
+Seed = {
+  create: function(number) {
+    return {
+      directives: function() {
+        for(i = 0; i < number; i++) {
+          createSampleDirective(i)
+        }
+      },
+      teachers: function() {
+        for(i = 0; i < number; i++) {
+          createSampleTeacher(i)
+        }
+      }
+    }
+  }
+}
+
+function createSampleDirective(i) {
+  var directive = Accounts.createUser({
+    username: 'aDirective' + i,
+    email: 'canotto90+directive' + i + '@gmail.com',
+    password: '1234'
+  })
+  Roles.addUsersToRoles(directive, ['admin', 'directive'])
+}
+
+function createSampleTeacher(i) {
+  var teacher = Accounts.createUser({
+    username: 'aTeacher' + i,
+    email: 'canotto90+teacher' + i + '@gmail.com',
+    password: '1234'
+  })
+  Roles.addUsersToRoles(teacher, ['admin', 'directive'])
+}
+
--- startup.js
+++ startup.js
@@ -1,17 +1,7 @@
 Meteor.startup(function() {
   if (Meteor.users.find().count() <= 0) {
-    var directive_id = Accounts.createUser({
-      username: 'aDirective',
-      email: 'canotto90@gmail.com',
-      password: '1234'
-    })
-    var teacher_id = Accounts.createUser({
-      username: 'aTeacher',
-      email: 'canotto90+1@gmail.com',
-      password: '1234'
-    })
-    Roles.addUsersToRoles(directive_id, ['admin', 'directive'])
-    Roles.addUsersToRoles(teacher_id, ['admin', 'teacher'])
+    Seed.create(1).directives()
+    Seed.create(2).teachers()
   }
   AccountsEntry.config({
     dashboardRoute: '/admin',
