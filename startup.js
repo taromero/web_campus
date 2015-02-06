55cd8f4bfaae4c1a421952d83dc14ce217a9fd3c
Tomas Romero <canotto90@gmail.com>
2015-02-06 01:37:29 -0300

    Fixed issue with login redirection

--- startup.js
+++ startup.js
@@ -1,11 +1,11 @@
 Meteor.startup(function() {
-  if (Meteor.users.find().count() <= 0) {
-    Seed.create(1).directives()
-    Seed.create(2).teachers()
-  }
   AccountsEntry.config({
     dashboardRoute: '/admin',
     passwordSignupFields: 'EMAIL_ONLY',
     showSignupCode: true
-  });
+  })
+  if (Meteor.users.find().count() <= 0) {
+    Seed.create(1).directives()
+    Seed.create(2).teachers()
+  }
 })
