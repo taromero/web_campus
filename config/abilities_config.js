7ddd6ffc736d07b968cd8c323a57e706b23e5a76
Tomas Romero <canotto90@gmail.com>
2015-02-08 03:17:03 -0300

    If no ability is defined, prevent action

--- config/abilities_config.js
+++ config/abilities_config.js
@@ -1,18 +1,30 @@
 // applies ability filters defined on each role's ability
 Collections.forEach(function(collection) {
-  var operations = ['anyFind']
-  operations.forEach(function(operation) {
-    collection.before[operation](provideUserAndRole(function(user, role, selector) {
-      if (safeNavigation()) {
-        return RoleAbilities[role].abilities[collection._name][operation](user, selector)
+  collection.before.anyFind(function(userId, selector) {
+    selector = selector || {}
+    return provideUserAndRole(userId, function(user, role) {
+      if (hasDefinedAbilities(role, collection, 'anyFind')) {
+        return RoleAbilities[role].abilities[collection._name].anyFind(user, selector)
+      } else {
+        selector._id = 'prevent_find'
       }
-      
-      function safeNavigation() {
-        return RoleAbilities[role] && RoleAbilities[role].abilities &&
-                RoleAbilities[role].abilities[collection._name] &&
-                RoleAbilities[role].abilities[collection._name][operation]
+    })
+  })
+
+  collection.before.insert(function(userId, doc) {
+    return provideUserAndRole(userId, function(user, role) {
+      if (hasDefinedAbilities(role, collection, 'save')) {
+        return RoleAbilities[role].abilities[collection._name].save(user, doc)
+      } else {
+        throw 'User not allowed to insert'
       }
-    }))
+    })
   })
 })
+
+function hasDefinedAbilities(role, collection, operation) {
+  return RoleAbilities[role] && RoleAbilities[role].abilities &&
+    RoleAbilities[role].abilities[collection._name] &&
+    RoleAbilities[role].abilities[collection._name][operation]
+}
 
