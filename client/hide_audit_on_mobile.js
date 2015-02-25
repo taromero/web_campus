14c6baa8a1002ee2ba37788e94c6b67a988cbc27
Tomas Romero <canotto90@gmail.com>
2015-02-25 20:49:44 -0300

    Hide audit columns on mobile

---
+++ client/hide_audit_on_mobile.js
@@ -0,0 +1,33 @@
+hide_audit_on_mobile = function() {
+  var table  = $('.dataTables_wrapper')
+  var headers = table.find('th')
+  var rows = table.find('tr')
+  var columns_to_hide_start = headers.length - 6 // 4 audit columns + edit and delete
+  var cths = columns_to_hide_start
+  var cols_to_hide = [cths++, cths++, cths++, cths++]
+
+  headers.each(function(i, header) {
+    cols_to_hide.forEach(function(cel_number) {
+      $(headers[cel_number]).addClass('hidden-xs')
+    })
+  })
+  rows.each(function(i, row) {
+    var cels = $(row).find('td')
+    cols_to_hide.forEach(function(cel_number) {
+      $(cels[cel_number]).addClass('hidden-xs')
+    })
+  })
+}
+
+$(function() {
+  hide_audit_on_mobile()
+  var counter = 0
+  var interval = setInterval(function() {
+    if (counter == 10) {
+      clearInterval(interval)
+      return
+    }
+    hide_audit_on_mobile()
+    counter++
+  }, 500)
+})
