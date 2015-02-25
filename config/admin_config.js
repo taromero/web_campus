964371104980289d29b843a76183149291967a81
Tomas Romero <canotto90@gmail.com>
2015-02-25 01:07:10 -0300

    Admin UI to spanish

--- config/admin_config.js
+++ config/admin_config.js
@@ -8,31 +8,37 @@ AdminConfig = {
   },
   collections: {
     Exams: {
+      label: 'Examenes',
       icon: 'mortar-board',
       color: 'green',
       auxCollections: [Subjects],
       tableColumns: Exams.autoTable
     },
     Subjects: {
+      label: 'Materias',
       icon: 'university',
       color: 'maroon',
       auxCollections: [Meteor.users, Courses],
       tableColumns: Subjects.autoTable
     },
     Courses: {
+      label: 'Cursos',
       icon: 'group',
       color: 'orange',
       tableColumns: Courses.autoTable
     },
     Reports: {
+      label: 'Reportes',
       icon: 'bar-chart',
       color: 'antiquewhite'
     },
     ExamScores: {
+      label: 'Notas',
       auxCollections: [Meteor.users, Exams],
       tableColumns: ExamScores.autoTable
     },
     Resources: {
+      label: 'Archivos',
       tableColumns: Resources.autoTable
     }
   }
