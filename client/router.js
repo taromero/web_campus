dfed52169169cffeee219ee0199ba8074abd3a12
Tomas Romero <canotto90@gmail.com>
2015-02-27 15:10:09 -0300

    Fix issue that needed 2 clicks  to log in

--- client/router.js
+++ client/router.js
@@ -1,3 +1,4 @@
+var isLoggedIn = Meteor.userId()
 Router.configure({
   onBeforeAction: function() {
     var _this = this
@@ -6,6 +7,14 @@ Router.configure({
     authCreateEdit()
 
     attempt_for_5_seconds_to(hide_audit_on_mobile)
+
+    Tracker.autorun(function() {
+      console.log(Meteor.userId())
+      if (isLoggedIn != Meteor.userId()) {
+        isLoggedIn = Meteor.userId()
+        Meteor.userId() &&_this.redirect('/') 
+      }
+    })
 
     this.waitOn = function() {
       return Collections.map(function(collection) {
