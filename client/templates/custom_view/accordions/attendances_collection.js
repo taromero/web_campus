d7343b4178e3b574cc1c1d47736328c026abe974
Tomas Romero <canotto90@gmail.com>
2015-02-28 20:39:27 -0300

    Handle item color on attendance state change

--- client/templates/custom_view/accordions/attendances_collection.js
+++ client/templates/custom_view/accordions/attendances_collection.js
@@ -4,6 +4,7 @@ Template.attendances_collection.helpers({
   }
 })
 
+var preventRowColorChange = false
 Template.attendances_collection.events({
   'click .lever': function(event) {
     // Apparently the switch component of Materialize doesn't change the value of the checkbox,
@@ -12,20 +13,40 @@ Template.attendances_collection.events({
     var hasActivatedSwitch = $switchInput.val() == 'off' //if it was off, a click activates it
     var newVal = !hasActivatedSwitch ? 'off' : 'on'
     var $relatedHalfAttendance = $switchInput.closest('.row').find('.half-attendance')
+    var collectionItem = $switchInput.closest('.collection-item')
+
     $switchInput.val(newVal)
 
+    if (preventRowColorChange) {
+      preventRowColorChange = false
+    } else {
+      collectionItem.attr('class', 'collection-item') //remove existing color
+      collectionItem.addClass(hasActivatedSwitch ? 'green lighten-3' : 'red lighten-2')
+    }
+
     if (hasActivatedSwitch && $relatedHalfAttendance.val() == 'on') {
+      preventRowColorChange = true
       $relatedHalfAttendance.click()
     }
+
   },
   'click .half-attendance': function(event) {
     var $checkbox = $(event.currentTarget)
     var hasActivatedHalfAttendance = $checkbox.val() == 'off' //if it was off, a click activates it
     var newVal = !hasActivatedHalfAttendance ? 'off' : 'on'
     var $relatedSwitch = $checkbox.closest('.row').find('.attendance-switch')
+    var collectionItem = $checkbox.closest('.collection-item')
     $checkbox.val(newVal)
 
+    if (preventRowColorChange) {
+      preventRowColorChange = false
+    } else {
+      collectionItem.attr('class', 'collection-item') //remove existing color
+      collectionItem.addClass(hasActivatedHalfAttendance ? 'deep-orange lighten-3' : 'red lighten-2')
+    }
+
     if (hasActivatedHalfAttendance && $relatedSwitch.val() == 'on') {
+      preventRowColorChange = true
       $relatedSwitch.siblings('.lever').click()
     }
   }
