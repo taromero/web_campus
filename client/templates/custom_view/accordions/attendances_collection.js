e0f8764fa332e9ae56eef2f46243e160a47abe3c
Tomas Romero <canotto90@gmail.com>
2015-03-01 15:34:56 -0300

    Improve responsive design of attendance

--- client/templates/custom_view/accordions/attendances_collection.js
+++ client/templates/custom_view/accordions/attendances_collection.js
@@ -1,6 +1,19 @@
 Template.attendances_collection.helpers({
   students: function() {
-    return Meteor.users.find({ course_id: this.course_id }).fetch(onlyStudents)
+    return Meteor.users.find({ course_id: this.course_id }).fetch(onlyStudents).map(addShortName)
+
+    function addShortName(student, index, students) {
+      var anotherWithSameLastName = students.filter(function(_student) {
+        return _student.lastName == student.lastName
+      }).length > 2
+
+      if (anotherWithSameLastName) {
+        student.profile.shortName = student.profile.lastName + ', ' + student.profile.firstName
+      } else {
+        student.profile.shortName = student.profile.lastName
+      }
+      return student
+    }
   }
 })
 
@@ -34,7 +47,7 @@ Template.attendances_collection.events({
     var $checkbox = $(event.currentTarget)
     var hasActivatedHalfAttendance = $checkbox.val() == 'off' //if it was off, a click activates it
     var newVal = !hasActivatedHalfAttendance ? 'off' : 'on'
-    var $relatedSwitch = $checkbox.closest('.row').find('.attendance-switch')
+    var $relatedSwitch = $checkbox.closest('.row').find('label:visible .attendance-switch') //label:visible is because we have 2 switches (1 for large and 1 for med-and-down)
     var collectionItem = $checkbox.closest('.collection-item')
     $checkbox.val(newVal)
 
