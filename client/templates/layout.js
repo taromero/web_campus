1b9e90fab42e9df145b4c636150caa0d573c2d30
Tomas Romero <canotto90@gmail.com>
2015-04-16 14:52:46 -0300

    Test

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -2,6 +2,7 @@ Template.header.rendered = function() {
   $(".button-collapse.hide-on-med-and-down").sideNav()
   $(".button-collapse.hide-on-large-only").sideNav({ closeOnClick: true })
   $('.modal-trigger').leanModal()
+  configurePushNotifications()
 }
 
 Template.header_links.events({
